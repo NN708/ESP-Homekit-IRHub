efa168e303a42846bd0aa02f1da5959b29cb027e
NN708 <yuqian_1998@126.com>
2019-07-09 14:11:01 +0800

    Add bridge accessory

--- ir_hub/accessory.c
+++ ir_hub/accessory.c
@@ -4,6 +4,7 @@
 #include "utilities/error_handler.h"
 
 #include "accessories/panasonic_ac.h"
+#include "accessories/bridge.h"
 
 uint8_t accessory_count = 0;
 
@@ -13,9 +14,15 @@ homekit_accessory_t** accessories_init() { //TODO: Read accessories from flash, 
         error_handler("Memory allocation failed.");
         return NULL;
     }
+    homekit_accessory_t* accessory;
 
     accessory_count++;
-    homekit_accessory_t* accessory = NEW_HOMEKIT_ACCESSORY(.id=accessory_count);
+    accessory = NEW_HOMEKIT_ACCESSORY(.id=accessory_count);
+    bridge_init(accessory);
+    accessories[accessory_count - 1] = accessory;
+
+    accessory_count++;
+    accessory = NEW_HOMEKIT_ACCESSORY(.id=accessory_count);
     panasonic_ac_init(accessory);
     accessories[accessory_count - 1] = accessory;
 
