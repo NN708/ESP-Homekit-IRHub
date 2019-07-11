c15624829ba2ad14bd9ab985702c6abeac955c46
NN708 <yuqian_1998@126.com>
2019-07-11 09:38:28 +0800

    Use esp-wifi-config

--- ir_hub/ir_hub.c
+++ ir_hub/ir_hub.c
@@ -4,34 +4,30 @@
 #include "task.h"
 #include "esp8266.h"
 
+#include <wifi_config.h>
 #include "utilities/error_handler.h"
 #include "accessory.h"
 #include "transmit.h"
 #include "config.h"
 
-static void wifi_init();
-void panasonic_ac_transmit(uint32_t code);
+void on_wifi_event(wifi_config_event_t event);
 
 homekit_server_config_t config;
 
 void user_init(void) {
     uart_set_baud(0, 115200);
     error_led_init();
-    wifi_init();
     transmit_init();
-
-    /* homekit init */
-    config.accessories = accessories_init();
-    config.password = "111-11-111";
-    homekit_server_init(&config);
+    wifi_config_init2("IRHub", "", on_wifi_event);
 }
 
-static void wifi_init() {
-    struct sdk_station_config wifi_config = {
-        .ssid = WIFI_SSID,
-        .password = WIFI_PASSWORD,
-    };
-    sdk_wifi_set_opmode(STATION_MODE);
-    sdk_wifi_station_set_config(&wifi_config);
-    sdk_wifi_station_connect();
+void on_wifi_event(wifi_config_event_t event) {
+    if(event == WIFI_CONFIG_CONNECTED) {
+        printf("Connected to WiFi\n");
+        config.accessories = accessories_init();
+        config.password = "111-11-111";
+        homekit_server_init(&config);
+    } else if(event == WIFI_CONFIG_DISCONNECTED) {
+        printf("Disconnected from WiFi\n");
+    }
 }
