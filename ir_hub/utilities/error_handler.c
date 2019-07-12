04188a88c52987e75c7bbfd9c33a77d0733251b7
NN708 <yuqian_1998@126.com>
2019-07-12 10:44:33 +0800

    Fix error_led

--- ir_hub/utilities/error_handler.c
+++ ir_hub/utilities/error_handler.c
@@ -6,16 +6,16 @@ const int led_gpio = 2;
 
 void error_led_init() {
     gpio_enable(led_gpio, GPIO_OUTPUT);
-    gpio_write(led_gpio, false);
+    gpio_write(led_gpio, true);
 }
 
 void error_handler(const char* message) {
     printf("Error: %s\n", message);
-    gpio_write(led_gpio, true);
+    gpio_write(led_gpio, false);
     while(true);
 }
 
 void warning_handler(const char* message) {
     printf("Warning: %s\n", message);
-    gpio_write(led_gpio, true);
+    gpio_write(led_gpio, false);
 }
