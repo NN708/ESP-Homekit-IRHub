ae0679611343fa6361b576b07b432ad949ad937e
NN708 <yuqian_1998@126.com>
2019-07-14 09:45:16 +0800

    Add reset_button

--- ir_hub/reset_button.c
+++ ir_hub/reset_button.c
@@ -2,18 +2,38 @@
 #include "FreeRTOS.h"
 #include "timers.h"
 #include "esp8266.h"
+
+#include <wifi_config.h>
+#include <homekit/homekit.h>
+#include <homekit/characteristics.h>
 #include "reset_button.h"
 
 void gpio_interrupt_handler(uint8_t gpio_num);
 
 const int button_gpio = 5;
+uint32_t press_time;
 
 void reset_button_init() {
     gpio_enable(button_gpio, GPIO_INPUT);
-    gpio_set_interrupt(button_gpio, GPIO_INTTYPE_EDGE_POS, gpio_interrupt_handler);
+    gpio_set_interrupt(button_gpio, GPIO_INTTYPE_EDGE_ANY, gpio_interrupt_handler);
 }
 
 void gpio_interrupt_handler(uint8_t gpio_num)
 {
-
+    if(gpio_read(button_gpio)) { // pos
+        press_time = sdk_system_get_time();
+    } else { // neg
+        uint32_t delta_time, now;
+        now = sdk_system_get_time();
+        if(press_time > now) {
+            delta_time = UINT32_MAX - press_time + now;
+        } else {
+            delta_time = now - press_time;
+        }
+        if(delta_time >= 5000000) { // 5 seconds
+            homekit_server_reset();
+            wifi_config_reset();
+            sdk_system_restart();
+        }
+    }
 }
