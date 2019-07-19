97d67997ff625997828d6609132f6d9b20b62769
NN708 <yuqian_1998@126.com>
2019-07-19 17:11:25 +0800

    Update panasonic ac Fix invalid value for current heating cooling state

--- ir_hub/accessories/panasonic_ac.c
+++ ir_hub/accessories/panasonic_ac.c
@@ -15,13 +15,12 @@
 #define FOOTER_SPACE 13900
 
 void panasonic_ac_identify(homekit_value_t _value);
-void panasonic_ac_heating_cooling_state_callback(homekit_characteristic_t *characteristic, homekit_value_t value, void *context);
+void panasonic_ac_target_heating_cooling_state_callback(homekit_characteristic_t *characteristic, homekit_value_t value, void *context);
 void panasonic_ac_target_temperature_callback(homekit_characteristic_t *characteristic, homekit_value_t value, void *context);
-
 uint32_t panasonic_ac_encode(homekit_accessory_t* accessory, bool toggle_power);
-
 void panasonic_ac_transmit(uint32_t code);
 void panasonic_ac_transmit_16(uint16_t code);
+uint8_t reverse_bits(uint8_t num, uint8_t length);
 
 void panasonic_ac_init(homekit_accessory_t* accessory) {
     accessory->category = homekit_accessory_category_thermostat;
@@ -29,14 +28,14 @@ void panasonic_ac_init(homekit_accessory_t* accessory) {
     homekit_add_service(accessory, NEW_HOMEKIT_SERVICE(THERMOSTAT, .primary=true));
 
     homekit_add_characteristic(accessory->services[0], NEW_HOMEKIT_CHARACTERISTIC(IDENTIFY, panasonic_ac_identify));
-    homekit_add_characteristic(accessory->services[0], NEW_HOMEKIT_CHARACTERISTIC(MANUFACTURER, "Ob"));
-    homekit_add_characteristic(accessory->services[0], NEW_HOMEKIT_CHARACTERISTIC(MODEL, "ObAC1"));
+    homekit_add_characteristic(accessory->services[0], NEW_HOMEKIT_CHARACTERISTIC(MANUFACTURER, "Panasonic"));
+    homekit_add_characteristic(accessory->services[0], NEW_HOMEKIT_CHARACTERISTIC(MODEL, "AC1")); // TODO: Fill in by user
     homekit_add_characteristic(accessory->services[0], NEW_HOMEKIT_CHARACTERISTIC(NAME, "Thermostat"));
     homekit_add_characteristic(accessory->services[0], NEW_HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "01"));
     homekit_add_characteristic(accessory->services[0], NEW_HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "0.1"));
 
     homekit_add_characteristic(accessory->services[1], NEW_HOMEKIT_CHARACTERISTIC(CURRENT_HEATING_COOLING_STATE, 0));
-    homekit_add_characteristic(accessory->services[1], NEW_HOMEKIT_CHARACTERISTIC(TARGET_HEATING_COOLING_STATE, 0, .callback=HOMEKIT_CHARACTERISTIC_CALLBACK(panasonic_ac_heating_cooling_state_callback)));
+    homekit_add_characteristic(accessory->services[1], NEW_HOMEKIT_CHARACTERISTIC(TARGET_HEATING_COOLING_STATE, 0, .callback=HOMEKIT_CHARACTERISTIC_CALLBACK(panasonic_ac_target_heating_cooling_state_callback)));
     homekit_add_characteristic(accessory->services[1], NEW_HOMEKIT_CHARACTERISTIC(CURRENT_TEMPERATURE, 25));
     homekit_add_characteristic(accessory->services[1], NEW_HOMEKIT_CHARACTERISTIC(TARGET_TEMPERATURE, 25, .callback=HOMEKIT_CHARACTERISTIC_CALLBACK(panasonic_ac_target_temperature_callback)));
     *(accessory->services[1]->characteristics[3]->min_value) = 16.0f;
@@ -46,50 +45,55 @@ void panasonic_ac_init(homekit_accessory_t* accessory) {
     homekit_add_characteristic(accessory->services[1], NEW_HOMEKIT_CHARACTERISTIC(NAME, "Thermostat"));
 }
 
-void panasonic_ac_heating_cooling_state_callback(homekit_characteristic_t *characteristic, homekit_value_t value, void *context) {
+
+void panasonic_ac_identify(homekit_value_t _value) {
+    printf("Panasonic AC identify\n");
+}
+
+void panasonic_ac_target_heating_cooling_state_callback(homekit_characteristic_t* characteristic, homekit_value_t value, void* context) {
     homekit_accessory_t* accessory = characteristic->service->accessory;
-    uint8_t state = value.int_value;
+    uint8_t target_state = value.int_value;
     homekit_characteristic_t* current_state_characteristic = characteristic->service->characteristics[0];
-    uint8_t current_state = current_state_characteristic->value.int_value;
-    bool toggle_power = (state == 0 && current_state != 0) || (current_state == 0 && state != 0);
-    panasonic_ac_transmit(panasonic_ac_encode(accessory, toggle_power));
-    current_state_characteristic->value = HOMEKIT_UINT8(state);
+
+    panasonic_ac_transmit(panasonic_ac_encode(accessory));
+    if(target_state == HEATING_COOLING_STATE_AUTO) target_state = HEATING_COOLING_STATE_OFF; // auto is not a valid value for current heating cooling state
+    current_state_characteristic->value = HOMEKIT_UINT8(target_state);
     homekit_characteristic_notify(current_state_characteristic, current_state_characteristic->value);
 }
-void panasonic_ac_target_temperature_callback(homekit_characteristic_t *characteristic, homekit_value_t value, void *context) {
+
+void panasonic_ac_target_temperature_callback(homekit_characteristic_t* characteristic, homekit_value_t value, void* context) {
     homekit_accessory_t* accessory = characteristic->service->accessory;
+    float target_temperature = value.float_value;
     homekit_characteristic_t* current_temperature_characteristic = characteristic->service->characteristics[2];
-    panasonic_ac_transmit(panasonic_ac_encode(accessory, false));
-    current_temperature_characteristic->value = HOMEKIT_FLOAT(value.float_value);
+
+    panasonic_ac_transmit(panasonic_ac_encode(accessory));
+    current_temperature_characteristic->value = HOMEKIT_FLOAT(target_temperature);
     homekit_characteristic_notify(current_temperature_characteristic, current_temperature_characteristic->value);
 }
 
-uint32_t panasonic_ac_encode(homekit_accessory_t* accessory, bool toggle_power) {
+uint32_t panasonic_ac_encode(homekit_accessory_t* accessory) {
+    uint8_t current_state = accessory->services[1]->characteristics[0]->value.int_value;
+    uint8_t target_state = accessory->services[1]->characteristics[1]->value.int_value;
+    uint8_t target_temperature = (int)accessory->services[1]->characteristics[3]->value.float_value;
     uint32_t code = 0x0F002F6C;
-    uint8_t state = accessory->services[1]->characteristics[1]->value.int_value;
-    uint8_t state_code = 0;
-    uint8_t temperature_code = 0;
-    if(state == 0) state = accessory->services[1]->characteristics[0]->value.int_value; // when changed to off, read current state
-    if(state == 0 || state == 3) { // off or auto
-        state_code = 3;
+    uint8_t state_code = 0, temperature_code = 0;
+
+    uint8_t state = ((target_state != HEATING_COOLING_STATE_OFF) ? target_state : current_state); // when changed to off, read current state
+    if(state == HEATING_COOLING_STATE_OFF || state == HEATING_COOLING_STATE_AUTO) {
+        state_code = 3; // auto
         temperature_code = 8;
-    } else {
-        uint8_t temperature = (int)accessory->services[1]->characteristics[3]->value.float_value;
-        temperature -= 15;
-        for(uint8_t i = 0; i < 4; i++) { // reverse temperature code
-            temperature_code <<= 1;
-            temperature_code += temperature % 2;
-            temperature >>= 1;
-        }
-        if(state == 1) { // heat
-            state_code = 1;
-        } else { // cool
-            state_code = 2;
-        }
+    } else if(state == HEATING_COOLING_STATE_HEAT) {
+        state_code = 1; // heat
+        temperature_code = reverse_bits(target_temperature - 15, 4);
+    } else if(state == HEATING_COOLING_STATE_COOL) {
+        state_code = 2; // cool
+        temperature_code = reverse_bits(target_temperature - 15, 4);
     }
+
+    bool toggle_power = ((target_state == HEATING_COOLING_STATE_OFF) && (current_state != HEATING_COOLING_STATE_OFF)) || ((current_state == HEATING_COOLING_STATE_OFF) && (target_state != HEATING_COOLING_STATE_OFF));
+    if(!toggle_power) code += 1 << 20;
     code += (uint32_t)state_code << 21;
     code += (uint32_t)temperature_code << 28;
-    if(!toggle_power) code += 1 << 20;
     return code;
 }
 
@@ -99,6 +103,7 @@ void panasonic_ac_transmit(uint32_t code) {
     panasonic_ac_transmit_16(high);
     panasonic_ac_transmit_16(low);
 }
+
 void panasonic_ac_transmit_16(uint16_t code) {
     uint8_t high = code >> 8, low = code;
     transmit_clear_time();
@@ -116,6 +121,12 @@ void panasonic_ac_transmit_16(uint16_t code) {
     transmit_space(FOOTER_SPACE);
 }
 
-void panasonic_ac_identify(homekit_value_t _value) {
-    printf("Panasonic AC identify\n");
+uint8_t reverse_bits(uint8_t num, uint8_t length) {
+    uint8_t reverse_num = 0;
+    for(uint8_t i = 0; i < length; i++) {
+        reverse_num <<= 1;
+        reverse_num += num % 2;
+        num >>= 1;
+    }
+    return reverse_num;
 }
--- ir_hub/utilities/homekit_utility.h
+++ ir_hub/utilities/homekit_utility.h
@@ -1,6 +1,13 @@
 #include <homekit/homekit.h>
 #include <homekit/characteristics.h>
 
+/* macros */
+#define HEATING_COOLING_STATE_OFF 0
+#define HEATING_COOLING_STATE_HEAT 1
+#define HEATING_COOLING_STATE_COOL 2
+#define HEATING_COOLING_STATE_AUTO 3
+
+/* dynamic memory functions */
 void homekit_add_accessory(homekit_server_config_t* config, homekit_accessory_t* accessory);
 void homekit_add_service(homekit_accessory_t* accessory, homekit_service_t* service);
 void homekit_add_characteristic(homekit_service_t* service, homekit_characteristic_t* characteristic);
